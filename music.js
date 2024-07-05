363222e4f26aeb613ba923daa32da4e4cdafacf6
Paramvir <77096659+sanwal-007@users.noreply.github.com>
2024-07-05 23:13:31 +0530

    Add files via upload

---
+++ music.js
@@ -0,0 +1,15 @@
+var audio = document.getElementById('audio');
+var playPauseBTN = document.getElementById('playPauseBTN');
+var count = 0;
+
+function playPause(){
+    if(count == 0){
+        count = 1;
+        audio.play();
+        playPauseBTN.innerHTML = "Pause &#9634;";
+    }else{
+        count = 0;
+        audio.pause();
+        playPauseBTN.innerHTML = "Play &#9654;";
+    }
+}
